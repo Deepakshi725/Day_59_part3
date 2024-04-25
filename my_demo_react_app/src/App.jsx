62e3c5c4d6687653cf4667964ff75c55ee737f57
Deepakshi275 <sharmapanchi99@gmail.com>
2024-04-25 17:45:04 +0530

    first commit

---
+++ my_demo_react_app/src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>hello Kalvium</h1>
+    </div>
+  );
+}
+
+export default App;
